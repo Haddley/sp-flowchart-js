186819c335558802ee073637796e4b0f76065804
Neil Haddley <neil@haddley.com>
2018-09-11 21:51:34 -0500

    intial commit

--- src/webparts/flowchart/components/IFlowchartProps.ts
+++ src/webparts/flowchart/components/IFlowchartProps.ts
@@ -1,7 +1,3 @@
-/*export interface IFlowchartProps {
-  description: string;
-}*/
-
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 import { DisplayMode } from "@microsoft/sp-core-library";
 import { IFlowchartWebPartProps } from "../FlowchartWebPart";
